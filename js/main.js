a9a5ac5186ae0d60c11dc286c92221bdc9252c1b
ChanghyeonO <ckdgus5189@gmail.com>
2022-10-12 01:54:16 +0900

    uploadimg

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-const add = require("./add")
+// const add = require("./add")
 
-console.log(add(1,2))
+// console.log(add(1,2))
+
+// import { add } from './add2'
+
+// console.log(add(1,2))
+
+console.log("hey")
